aab1cfba439fd480f285896a211a35cfc3a0b546
João Vitor Diniz Alves <diniz.vitor.alves@gmail.com>
2026-06-24 10:10:47 -0300

    ÃTratamento de Erros

---
+++ aula_exceptions/src/aula_exceptions/Divisao.java
@@ -0,0 +1,47 @@
+package aula_exceptions;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Divisao {
+
+	static Scanner ler = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		int dividendo = 0;
+		int divisor = 0;
+		boolean loop = true;
+		
+		do {
+			
+			try {
+				System.out.println("Digite o Dividendo: ");
+				dividendo = ler.nextInt();
+						
+				System.out.println("Digite o Divisor: ");
+				divisor = ler.nextInt();
+				
+				loop = false;
+						
+				divide(dividendo, divisor);
+			} catch(InputMismatchException error) { // esse "e" é abreviação do error 
+				System.err.println("Erro de digitação: " + error.getMessage());
+				ler.nextLine();
+			}catch(ArithmeticException e) {
+				System.err.println("Erro de digitação: " + e.getMessage());
+				ler.nextLine();
+			}finally {
+				System.out.println("Obrigado pela preferência");
+			}
+			
+		} while(loop);
+		
+		
+	}
+
+	public static void divide(int dividendo, int divisor) {
+		System.out.println("Divisão = " + (dividendo / divisor));
+	}
+
+}
